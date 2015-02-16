a7459ce18c99ffabe66ad2fec1f9958ccd5db8cc
Ubuntu <ubuntu@ip-172-31-34-222.us-west-2.compute.internal>
2015-02-16 05:18:39 +0000

    second

--- SendRequest.py
+++ SendRequest.py
@@ -1,9 +1,11 @@
 import requests
+import sys
 import time
 import logging
 from BeautifulSoup import BeautifulSoup
 from urlparse import urlparse
-from pylab import *
+import socket
+#from pylab import *
 
 #Data will be parameter if it's GET
 #Data will be data if it's POST
@@ -12,23 +14,31 @@ def sendRequest(url,method,data,headers):
 	logger = logging.getLogger('sendRequest')
 	method = method.upper()
 	try:
+		o = urlparse(url)
+		host = o.netloc
+		starting_time = time.time()
+		ip = socket.gethostbyname(host)
+		end_time = time.time()
+		dns_time = end_time - starting_time
+		
 		if method == "GET":
 			r = None
 			starting_time = time.time()
 			r = requests.get(url,params=data,headers=headers,timeout=20)
 			response = r.text
 			end_time = time.time()
-			return response, end_time-starting_time
+			return response, end_time-starting_time,dns_time
 		elif method == "HEAD":
 			starting_time = time.time()
 			r = requests.head(url,params=data,headers=headers,timeout=20)
 			resp_head = r.headers
 			end_time = time.time()
 			#logger.info(resp_head)
-			return resp_head,end_time-starting_time
+			return resp_head,end_time-starting_time,dns_time
 	except Exception as e:
 		logger.error("SendRequest Exception: "+str(e))
-		return None, NOne		
+		return None, None		
+
 
 def extractURLFromWebPageWithHost(body,host):
 	soup = BeautifulSoup(body)
@@ -45,9 +55,10 @@ def extractURLFromWebPageWithHost(body,host):
 		for item in rs:
 			url = item.get('href')
 			try:
+				print url
 				o = urlparse(url)
-				if (o != None) and (o.netloc == host):
-					#print url
+				if (o != None) and (host in o.netloc):
+					print url
 					final_rs.append(url)
 			except Exception as e:
 				logger.warning("Exception in parsing URL "+str(url) +" "+str(e))
@@ -57,20 +68,25 @@ if __name__=="__main__":
 	logging.basicConfig(filename="logs", filemode='w', level=logging.ERROR, \
                         format='%(asctime)s\t%(levelname)s\t%(message)s', datefmt='[%Y-%m-%d %I:%M:%S]')
 	logger = logging.getLogger('sendRequest')
-	headers = { 'User-Agent' : "Mozilla" }
-	data = {""}
-	url = "http://www.sina.com.cn"
+	headers = { 'User-Agent' : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.104 Safari/537.36" }
+	data = {"arg1":"val2","arg2":"val2"}
+	url = sys.argv[1]
+	o = urlparse(url)
+	host = sys.argv[2]
 	
-	rsp, interval = sendRequest(url,"GET",None,headers)
-	logger.info("GET Content-Len:"+str(len(rsp))+"Time:"+str(interval))
+	rsp, interval, dns_time = sendRequest(url,"GET",None,headers)
+	print rsp
+	logger.error("GET  %40s Content-Len: %7d Interval: %.6f DNS: %f",url,len(rsp),interval,dns_time)
 	size_type_map = {}
 	size_type_map[len(rsp)] = interval
-	urls = extractURLFromWebPageWithHost(rsp,"www.sina.com.cn")
+	urls = extractURLFromWebPageWithHost(rsp,host)
 	for url in urls:
-		rsp, interval = sendRequest(url,"HEAD",None,headers)
-		logger.error("HEAD %s Content-Len: %7d Interval: %f ",url,len(rsp),interval)
-		rsp, interval = sendRequest(url,"GET",None,headers)
-		logger.error("HEAD %s Content-Len: %7d Interval: %f ",url,len(rsp),interval)
+		rsp, interval,dns_time = sendRequest(url,"HEAD",None,headers)
+		if rsp == None:
+			continue
+		logger.error("HEAD %40s Content-Len: %7d Interval: %.6f DNS: %f",url,len(rsp),interval,dns_time)
+		rsp, interval, dns_time = sendRequest(url,"GET",None,headers)
+		logger.error("GET  %40s Content-Len: %7d Interval: %.6f DNS: %f",url,len(rsp),interval,dns_time)
 		size_type_map[len(rsp)] = interval
 		
 	#x = []
